426eb88ff8cf7f938101bb74d988c2199c1983b2
Bess Sadler <bess.sadler@princeton.edu>
2024-02-23 16:42:25 -0500

    First pass at generator and tests (#1)

--- lib/blacklight_lando.rb
+++ lib/blacklight_lando.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 require_relative "blacklight_lando/version"
+require_relative "generators/lando_generator"
 
 module BlacklightLando
   class Error < StandardError; end
-  # Your code goes here...
 end
