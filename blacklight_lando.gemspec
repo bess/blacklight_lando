6694fbf327f944b77417bbc24bb012eb6efdd263
Bess Sadler <bess.sadler@princeton.edu>
2024-02-23 17:09:23 -0500

    Fix CI

--- blacklight_lando.gemspec
+++ blacklight_lando.gemspec
@@ -38,6 +38,7 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "byebug"
   spec.add_development_dependency "generator_spec"
   spec.add_development_dependency "rspec"
+  spec.add_development_dependency "rubocop-rspec"
 
   # For more information and examples about making a new gem, check out our
   # guide at: https://bundler.io/guides/creating_gem.html
--- spec/generators/blacklight_lando/lando_generator_spec.rb
+++ spec/generators/blacklight_lando/lando_generator_spec.rb
@@ -8,13 +8,13 @@ TMP_FILE = File.expand_path("../tmp", __dir__)
 
 RSpec.describe BlacklightLando::LandoGenerator, type: :generator do
   destination TMP_FILE
-  before(:all) do
+  before do
     FileUtils.rm_rf(TMP_FILE)
     prepare_destination
     run_generator
   end
 
-  it "makes expected files" do
+  it "makes expected files" do # rubocop:disable RSpec/ExampleLength
     expect(destination_root).to have_structure {
       directory "config" do
         file "lando_env.rb"
