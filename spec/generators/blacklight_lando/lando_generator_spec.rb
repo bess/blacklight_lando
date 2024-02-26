082615ba33339de43fed68ab4583bb0a00282e95
Bess Sadler <bess.sadler@princeton.edu>
2024-02-26 09:29:56 -0500

    Use an initializer instead of modifying application.rb (#4)

--- spec/generators/blacklight_lando/lando_generator_spec.rb
+++ spec/generators/blacklight_lando/lando_generator_spec.rb
@@ -11,12 +11,12 @@ RSpec.describe BlacklightLando::LandoGenerator, type: :generator do
   before do
     FileUtils.rm_rf(TMP_FILE)
     prepare_destination
-    run_generator
   end
 
   it "makes expected files" do # rubocop:disable RSpec/ExampleLength
+    run_generator
     expect(destination_root).to have_structure {
-      directory "config" do
+      directory "config/initializers" do
         file "lando_env.rb"
       end
       file ".lando.yml"
